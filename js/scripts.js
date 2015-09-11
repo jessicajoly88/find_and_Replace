4242307ee0f939a443480b4679fbb416b252dc6f
Jessica <jessicajoly88@gmail.com>
2015-09-11 11:37:08 -0700

    Add jQuery functionality and works in browswer.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-var findReplace = function(string, search, newWord) {
-  var newString= string.split(' ');
+var findReplace = function(string, search, newWord) {  
+  var newString = string.split(' ');
 
   for(var i = 0; i <newString.length; i++ ) {
   	if (newString[i] === search) {
@@ -8,3 +8,19 @@ var findReplace = function(string, search, newWord) {
   }
   return newString.join(" ");
 };
+
+$(document).ready(function() {
+  $('form#find_replace').submit(function(event) {
+  	var string = ($("input#string").val());
+  	var search = ($("input#search").val());
+  	var newWord = ($("input#newWord").val());
+  	var result = findReplace(string,search,newWord);
+
+  $(".newSentence").text("");
+  $(".newSentence").text(result);
+  
+  $("#result").show();
+    event.preventDefault();	
+  });
+});
+
